a4d8fb4540e1945558242e3bb58e98034d8efa25
Shubham Kumar <shubhamkhantwal01@gmail.com>
2022-01-16 11:01:24 +0530

    Updates

---
+++ precache-manifest.cb70dda50180e0316d4ecfaf6c9dd70a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b6d86866df8bccf163bd6153dd4718f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c1f16dfcf32ccbb4abe4",
+    "url": "/static/css/main.25e29ad3.chunk.css"
+  },
+  {
+    "revision": "9433256fb35c32272601",
+    "url": "/static/js/2.66c5446c.chunk.js"
+  },
+  {
+    "revision": "a90bb751b102ed9d8f9443a23ededd32",
+    "url": "/static/js/2.66c5446c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1f16dfcf32ccbb4abe4",
+    "url": "/static/js/main.cff4c178.chunk.js"
+  },
+  {
+    "revision": "406db1bb00fce4daf1b6",
+    "url": "/static/js/runtime-main.2184c67b.js"
+  }
+]);
